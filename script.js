df013d64e2fdde29658e749584fd74de7cf1a76f
ArthurVillela <arthur.villela1@gmail.com>
2024-05-09 15:31:43 +0200

    first commit

--- script.js
+++ script.js
@@ -124,8 +124,8 @@ function deal(){
     cardImg3.src = "./Cards/" + dealerCard1 + ".png"
     dealerHandDiv.appendChild(Back)
 
-    Back.classname = "back"
-    cardImg3.classname = "front"
+    Back.classList.add("back")
+    cardImg3.classList.add("front")
 
     cardImg4.src = "./Cards/" + dealerCard2 + ".png"
     dealerHandDiv.append(cardImg4)
